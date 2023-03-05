1f5e04f58699ad2369facfc1f9820d370157c2b9
Maryna Karamanova <karamanova91@gmail.com>
2023-03-05 13:55:32 +0200

    Updated FeedbackButton by removing useState

--- src/components/feedbackoptions/FeedbackButton.jsx
+++ src/components/feedbackoptions/FeedbackButton.jsx
@@ -1,28 +1,24 @@
 import PropTypes from 'prop-types';
 import { OptionButtons } from './FeedbackOptions.styled';
-import { useState } from 'react';
 
 const FeedbackButton = ({ feedback, onLeaveFeedback }) => {
-  const [buttonState, setButtonState] = useState(0);
-  
-  const handleButtonClick = () => {
-    setButtonState(buttonState + 1);
-    onLeaveFeedback(feedback, buttonState);
-  };
+const handleButtonClick = () => {
+onLeaveFeedback(feedback);
+};
 
-  return (
-    <OptionButtons
-      type="button"
-      data-feedback={feedback}
-      onClick={handleButtonClick}
-    >
-      {feedback}
-    </OptionButtons>
-  );
+return (
+<OptionButtons
+   type="button"
+   data-feedback={feedback}
+   onClick={handleButtonClick}
+ >
+{feedback}
+</OptionButtons>
+);
 };
 
 FeedbackButton.propTypes = {
-  feedback: PropTypes.string.isRequired,
-  onLeaveFeedback: PropTypes.func.isRequired,
+feedback: PropTypes.string.isRequired,
+onLeaveFeedback: PropTypes.func.isRequired,
 };
 export default FeedbackButton;
