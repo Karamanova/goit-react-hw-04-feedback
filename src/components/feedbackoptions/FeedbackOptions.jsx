aa012bbe487f000cee3322ed6e18fbc729d204f6
Maryna Karamanova <karamanova91@gmail.com>
2023-03-05 14:00:51 +0200

    Updated FeedbackOptions by removing useCallback

--- src/components/feedbackoptions/FeedbackOptions.jsx
+++ src/components/feedbackoptions/FeedbackOptions.jsx
@@ -1,30 +1,28 @@
 import PropTypes from 'prop-types';
 import { FeedbackBox } from './FeedbackOptions.styled';
 import FeedbackButton from './FeedbackButton';
-import { useCallback } from 'react';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-  const onLeaveFeedbackCallback = useCallback(
-    (feedback, buttonState) => {
-      onLeaveFeedback(feedback, buttonState);
-    },
-    [onLeaveFeedback]
-  );
-
-  return (
-    <FeedbackBox>
-      {options.map((option, id) => (
-        <FeedbackButton
-          feedback={option}
-          onLeaveFeedback={onLeaveFeedbackCallback}
-          key={id}
-        />
-      ))}
-    </FeedbackBox>
-  );
+return (
+<FeedbackBox>
+{options.map((option, id) => (
+<FeedbackButton
+       feedback={option}
+       onLeaveFeedback={onLeaveFeedback}
+       key={id}
+     />
+))}
+</FeedbackBox>
+);
 };
 
 FeedbackOptions.propTypes = {
-  options: PropTypes.arrayOf(PropTypes.string),
-  onLeaveFeedback: PropTypes.func.isRequired,
+options: PropTypes.arrayOf(PropTypes.string),
+onLeaveFeedback: PropTypes.func.isRequired,
 };
+
+
+
+
+
+
